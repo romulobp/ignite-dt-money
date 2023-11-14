744777427c786bd5b48487f86eb595cf4692bf28
Rômulo Petkovicz <romulo.petkovicz@meta.com.br>
2023-11-14 20:57:24 -0300

    03

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import {GlobalStyle} from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
+          <GlobalStyle/>
       </header>
     </div>
   );
---
+++ src/styles/global.ts
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #121214;
+    --shape: #202024;
+    --second-shape: #29292E;
+    --third-shape: #323238;
+
+    --placeholder: #7C7C8A;
+    --base-text: #C4C4CC;
+    --titles: #E1E1E6;
+    --white: #FFFFFF;
+
+    --green-dark: #015F43;
+    --green: #00875F;
+    --green-light: #00B37E;
+
+    --red-dark: #AA2834;
+    --red: #F75A68;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
